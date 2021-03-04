3d3d2a4f7ea5d52e0beb41fb83026a8ea4a0fc32
Adrian <ajim1989@gmail.com>
2021-03-03 16:07:49 -0800

    Implemented laser and collision

--- main.py
+++ main.py
@@ -31,6 +31,8 @@ WHITE = (255, 255, 255)
 
 
 class Ship:
+    COOL_DOWN = 30
+
     def __init__(self, x, y, health=100):
         self.x = x
         self.y = y
@@ -42,6 +44,30 @@ class Ship:
 
     def draw(self, window):
         window.blit(self.ship_img, (self.x, self.y))
+        for laser in self.lasers:
+            laser.draw(window)
+
+    def shoot(self):
+        if self.cool_down_counter == 0:
+            laser = Laser(self.x, self.y, self.laser_img)
+            self.lasers.append(laser)
+            self.cool_down_counter = 1
+
+    def cool_down(self):
+        if self.cool_down_counter >= self.COOL_DOWN:
+            self.cool_down_counter = 0
+        elif self.cool_down_counter > 0:
+            self.cool_down_counter += 1
+
+    def move_laser(self, vel, obj):
+        self.cool_down()
+        for laser in self.lasers:
+            laser.move(vel)
+            if laser.off_screen(HEIGHT):
+                self.lasers.remove(laser)
+            elif laser.collision(obj):
+                obj.health -= 10
+                self.lasers.remove(laser)
 
     def get_width(self):
         return self.ship_img.get_width()
@@ -57,6 +83,18 @@ class Player(Ship):
         self.laser_img = YELLOW_LASER
         self.mask = pygame.mask.from_surface(self.ship_img)
         self.max_health = health
+
+    def move_laser(self, vel, objs):
+        self.cool_down()
+        for laser in self.lasers:
+            laser.move(vel)
+            if laser.off_screen(HEIGHT):
+                self.lasers.remove(laser)
+            else:
+                for obj in objs:
+                    if laser.collision(obj):
+                        objs.remove(obj)
+                        self.lasers.remove(laser)
 
 
 class Enemy(Ship):
@@ -75,6 +113,32 @@ class Enemy(Ship):
         self.y += vel
 
 
+class Laser:
+    def __init__(self, x, y, img):
+        self.x = x
+        self.y = y
+        self.img = img
+        self.mask = pygame.mask.from_surface(self.img)
+
+    def draw(self, window):
+        window.blit(self.img, (self.x, self.y))
+
+    def move(self, vel):
+        self.y += vel
+
+    def off_screen(self, height):
+        return not(height >= self.y >= 0)
+
+    def collision(self, obj):
+        return collide(obj, self)
+
+
+def collide(obj1, obj2):
+    offset_x = obj2.x - obj1.x
+    offset_y = obj2.y - obj1.y
+    return obj1.mask.overlap(obj2.mask, (offset_x, offset_y)) is not None  # (x, y): Point of intersection
+
+
 def main():
     run = True
     fps = 60
@@ -88,6 +152,8 @@ def main():
     enemy_vel = 1
 
     player_vel = 5
+
+    laser_vel = 5
 
     player = Player(300, 650)
 
@@ -154,12 +220,17 @@ def main():
             player.y -= player_vel
         if (keys[pygame.K_s] or keys[pygame.K_DOWN]) and player.y + player_vel + player.get_height() < HEIGHT:  # Down
             player.y += player_vel
+        if keys[pygame.K_SPACE]:
+            player.shoot()
 
         for enemy in enemies[:]:
             enemy.move(enemy_vel)
+            enemy.move_laser(laser_vel, player)
             if enemy.y + enemy.get_height() > HEIGHT:
                 lives -= 1
                 enemies.remove(enemy)
+
+        player.move_laser(-laser_vel, enemies)
 
 
 main()
